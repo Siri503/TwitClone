7d7a4a3f65ebe9e25df2d7edce58a440f28ae551
Siri503 <sirikanthiachanta@gmail.com>
2024-04-30 15:42:36 +0530

    signup controller

---
+++ backend/models/user.model.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required:true,
+        unique:true,
+    },
+    fullname:{
+       type: String,
+       required:true,
+    },
+    password:{
+       type: String,
+       required:true,
+       minlength:6,
+    },
+    email:{
+        type: String,
+        required:true,
+        unique:true,
+    },
+    followers:[
+        {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        default:[],
+        }
+    ],
+    following:[
+        {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        default:[],
+       }
+    ],
+    profileImg:{
+        type:String,
+        default:"",
+    },
+    coverImg:{
+        type:String,
+        default:"",
+    },
+    bio:{
+        type:String,
+        default:"",
+    },
+    link:{
+        type:String,
+        default:"",
+    }
+
+},{timestamps:true});
+const User=mongoose.model("User",userSchema);
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express"
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
-
 const app=express()
 dotenv.config()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
 app.use("/api/auth",authRoutes);
 const PORT=process.env.port||5000;
+
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`)
     connectMongoDB();
