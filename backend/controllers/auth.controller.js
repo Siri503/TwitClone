a8e5fc2ca2e7a7f7434964bd1a868e9fd20745e4
Siri503 <sirikanthiachanta@gmail.com>
2024-04-30 16:01:35 +0530

    login 🔓

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const signup=async(req,res)=>{
     }
     const salt=await bcrypt.genSalt(10);
     const hashedPassword=await bcrypt.hash(password,salt);
-    const newUser=new User({
+    const user=new User({
         fullname,
         username,
         email,
@@ -50,9 +50,29 @@ export const signup=async(req,res)=>{
     }
 }
 export const login=async(req,res)=>{
-    res.json({
-       data:"You hit the login endpoint",
-    })
+   try{
+     const {username,password}=req.body;
+     const user=await User.findOne({username});
+     const isPasswordCorrect=await bcrypt.compare(password,user?.password||"")
+     if(!user || !isPasswordCorrect){
+        return res.status(400).json({error:"Invalid username or password"})
+     }
+     generateTokenAndSetCookie(user._id,res);
+     res.status(200).json({
+        _id:user._id,
+            fullname:user.fullname,
+            username:user.username,
+            email:user.email,
+            followers:user.followers,
+            following:user.following,
+            profileImg:user.profileImg,
+            coverImg:user.coverImg,
+     })
+
+   }catch(error){
+    console.log("Error in login controller",error.message)
+    res.status(500).json({error:"Internal Server Error"})
+   }
 }
 export const logout=async(req,res)=>{
     res.json({
