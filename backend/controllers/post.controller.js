eb38c2b908c6d0c9ac7bff8f08812b0683884b4f
Siri503 <sirikanthiachanta@gmail.com>
2024-05-02 00:01:32 +0530

    comment💬

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -50,3 +50,24 @@ export const deletePost=async(req,res)=>{
        return res.status(500).json({error:"Internal Server Error"});
     }
 }
+export const commentOnPost=async(req,res)=>{
+  try{
+  const {text}=req.body;
+  const postId=req.params.id;
+  const userId=req.user._id;
+  if(!text){
+    return res.status(404).json({error:"Text field is required"})
+  }
+  const post=await Post.findById(postId)
+  if(!post){
+    return res.status(404).json({error:"Post not found"})
+  }
+  const comment={user:userId,text}
+  post.comments.push(comment);
+  await post.save();
+  res.status(200).json(post)
+}catch(error){
+  console.log("Error in commenting the post:",error.message)
+   res.status(500).json({error:"Internal Server Error"})
+}
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost,deletePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost,deletePost } from "../controllers/post.controller.js";
 const router=express.Router();
 
 router.post("/create",protectRoute,createPost);
 router.delete("/:id",protectRoute,deletePost);
+router.post("/comment/:id",protectRoute,commentOnPost);
 
 export default router;
