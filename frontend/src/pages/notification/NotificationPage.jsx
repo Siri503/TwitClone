fd777747dc6bb6b820219977676c70f34ac81919
Siri503 <sirikanthiachanta@gmail.com>
2024-05-21 19:25:57 +0530

    🔔📣Notification

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,37 +1,55 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-
+import {useQuery,useMutation,useQueryClient} from "@tanstack/react-query";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import toast from "react-hot-toast";
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
-
+	// const deleteNotifications = () => {
+	// 	alert("All notifications deleted");
+	// };
+	const queryClient=useQueryClient();
+    const {data:notifications,isLoading}=useQuery({
+		queryKey:["notifications"],
+		queryFn:async()=>{
+			try{
+             const res=await fetch("/api/notifications");
+			 const data=await res.json();
+			 if(!res.ok){
+				throw new Error(data.error||"Something new wrong!");
+			 }
+			 return data;
+			}catch(error){
+				throw new Error(error.message);
+			}
+		},
+	});
+	const {mutate:deleteNotifications}=useMutation({
+		mutationFn:async()=>{
+			try{
+				const res=await fetch("/api/notifications",{
+					method:"DELETE",
+				});
+				const data=await res.json();
+				if(!res.ok){
+					throw new Error(data.error||"Something went wrong");
+				}
+				return data;
+			}catch(error){
+				throw new Error(error);
+			}
+		},
+		onSuccess:()=>{
+          toast.success("Notifications deleted successfully");
+		  queryClient.invalidateQueries({queryKey:["notifications"]});
+		},
+		onError:(error)=>{
+			toast.error(error.message);
+		}
+	})
 	return (
 		<>
 			<div className='flex-[4_4_0] border-l border-r border-gray-700 min-h-screen'>
