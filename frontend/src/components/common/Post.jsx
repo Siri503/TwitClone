38acd42bfb63ac01acc4bf2d9f03213181f0ab2c
Siri503 <sirikanthiachanta@gmail.com>
2024-05-21 18:12:24 +0530

    comment

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,6 +9,7 @@ import {useQuery, useQueryClient} from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
 import { useMutation } from "@tanstack/react-query";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -68,12 +69,39 @@ const Post = ({ post }) => {
 		}
 
 	})
+	const {mutate:commentPost,isPending:isCommenting}=useMutation({
+       mutationFn:async()=>{
+         try{
+			const res=await fetch(`/api/posts/comment/${post._id}`,{
+				method:"POST",
+				headers:{
+					"Content-Type":"application/json",
+				},
+				body:JSON.stringify({text:comment}),
+			})
+           const data=await res.json();
+		   if(!res.ok){
+			throw new Error(data.error||"Something went wrong!");
+		   }
+		   return data;
+		 }catch(error){
+			throw new Error(error);
+		 }
+	   },
+	   onSuccess:()=>{
+          toast.success("Comment posted successfully");
+		  setComment("");
+		  queryClient.invalidateQueries({queryKey:["posts"]});
+	   },
+	   onError:(error)=>{
+		toast.error(error.message);
+	   },
+	})
 	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id);
+	const isLiked = post.likes.includes(authUser?._id);
 	const isMyPost = authUser?._id === post?.user?._id;
-	const formattedDate = "1h";
+	const formattedDate = formatPostDate(post.createdAt);
 
-	const isCommenting = false;
 
 	const handleDeletePost = () => {
 		deletePost();
@@ -81,6 +109,8 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if(isCommenting) return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
