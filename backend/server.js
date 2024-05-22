714c1ef9223a9bebf9fef042cb962997d75956cf
Siri503 <sirikanthiachanta@gmail.com>
2024-05-22 10:38:11 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,13 +9,16 @@ import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
-const app=express()
+
 dotenv.config()
 cloudinary.config({
     cloud_name:process.env.Cloud_name,
     api_key:process.env.API_key,
     api_secret:process.env.API_secret,
  });
+const app=express()
+const PORT=process.env.port||5000;
+const __dirname=path.resolve()
 app.use(express.json({limit:"5mb"}))
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser())
@@ -22,7 +26,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes);
-const PORT=process.env.port||5000;
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path,resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`)
